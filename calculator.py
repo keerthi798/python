ef4bb2e6ae5bd17f361bd6a7017bca05a829fc03
keerthi <keerthisree798@gmail.com>
2022-12-18 11:25:59 +0530

    second repository

---
+++ calculator.py
@@ -0,0 +1,22 @@
+a=int(input("enter a first number"))
+b=int(input("enter a second number:"))
+ch=int(input("enter your choice:"))
+switch(ch)
+case 1:
+print("sum of two numbers:",c)
+c=a+b
+break
+case 2:
+print("difference of two numbers:",d)
+d=a-b
+break
+case 3:
+print("multiplication of two numbers:",f)
+f=a*b
+break
+case 4:
+print("division of two numbers:",g)
+g=a/b
+break
+
+
